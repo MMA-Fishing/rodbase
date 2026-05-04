f2651ac8786c991566a62b31568df3593039417e
Kandle <vmflux_hk@outlook.com>
2026-05-04 16:56:20 +0800

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,12 @@ import "./styles/catalogue.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 
 
